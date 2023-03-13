69ac2047d57c1cdb32304a5373dde32315b00be5
carter-123 <wilsonc2@middleton.school.nz>
2023-03-14 10:12:36 +1300

    changes

---
+++ movie_tickets_v1.py
@@ -0,0 +1,20 @@
+""" Movie theatre ticketing system - v1
+Welcome screen and variables
+Created by Carter Wilson
+"""
+
+
+# Component 1 - Welcome screen and set up variables
+def sell_ticket():
+    print("********** FanFare Movies - ticketing system **********\n")
+
+    adult_tickets = 0
+    student_tickets = 0
+    child_tickets = 0
+    gift_vouchers = 0
+    tickets_sold = 0
+    total_sales = 0
+
+
+# Main routine
+sell_ticket()
